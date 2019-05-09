4379878b75373fd99d4723e3cdddf7b83fa32839
bloussou <brieuc.loussouarn@student.ecp.fr>
2019-05-08 23:32:31 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-mariomariage
+# super-mario-message
+
+## Live
+
+![Image](https://gogoprog.github.io/super-mario-message/video.gif)
+
+Enter your own message here: [Live demo](http://gogoprog.github.io/super-mario-message)
+
+## Install 
+
+- Install haxe on your computer [download](https://haxe.org/download/)
+- Run `haxe build.hxml`
+- if cors error :
+    1. install python3
+    2. run `python3 -m http.server` in this folder
+    3. open `http://0.0.0.0:8000/src/` in your browser web
+
---
+++ deps/whiplash/src/whiplash/Application.hx
@@ -0,0 +1,120 @@
+package whiplash;
+
+import ash.fsm.EngineState;
+import ash.fsm.EngineStateMachine;
+import js.jquery.*;
+import js.Browser.window;
+import js.Browser.document;
+
+class Application {
+    public var engine:ash.core.Engine;
+    public var uiEngine:ash.core.Engine;
+#if phaser
+    var game:phaser.Game;
+#end
+    var esm:EngineStateMachine;
+    var ingameEsm:EngineStateMachine;
+    var uiEsm:EngineStateMachine;
+    var pages:js.uipages.Group;
+    var statePageMap:Map<String, String>;
+
+    var deltaTime:Float;
+    var timeFactor:Float = 1;
+
+    public function new(width:Int = 800, height:Int = 600, parent:String = "body") {
+        new JQuery(window).on("load", function() {
+            whiplash.Lib.init(width, height, parent, {preload:preload, create:create, update:update});
+            whiplash.Input.setup(document.querySelector(parent));
+            uiEngine = new ash.core.Engine();
+            engine = whiplash.Lib.ashEngine;
+#if phaser
+            game = whiplash.Lib.phaserGame;
+#end
+            esm = new EngineStateMachine(engine);
+            ingameEsm = new EngineStateMachine(engine);
+            uiEsm = new EngineStateMachine(uiEngine);
+            statePageMap = new Map<String, String>();
+            onGuiLoaded();
+        });
+    }
+
+    function preload():Void {
+#if phaser
+        DataManager.preload(game);
+#end
+    }
+
+    function create():Void {
+#if phaser
+        AudioManager.init(game);
+#end
+    }
+
+    function update():Void {
+        deltaTime = whiplash.Lib.getDeltaTime();
+        deltaTime *= timeFactor;
+        whiplash.Input.update();
+        engine.update(deltaTime);
+        uiEngine.update(deltaTime);
+        whiplash.Input.postUpdate();
+    }
+
+    function createState(name:String):EngineState {
+        var state = new EngineState();
+        esm.addState(name, state);
+        return state;
+    }
+
+    function createIngameState(name:String):EngineState {
+        var state = new EngineState();
+        ingameEsm.addState(name, state);
+        return state;
+    }
+
+    function createUiState(name:String, page:Null<String>):EngineState {
+        var state = new EngineState();
+        uiEsm.addState(name, state);
+
+        if(page != null) {
+            statePageMap.set(name, page);
+        }
+
+        return state;
+    }
+
+    function onResize() {
+    }
+
+    function onGuiLoaded() {
+        pages = js.uipages.Lib.createGroup(new JQuery(".pages"));
+        pages.showPage(".default");
+        onResize();
+    }
+
+    public function changeState(stateName) {
+        engine.updateComplete.addOnce(function() {
+            esm.changeState(stateName);
+        });
+    }
+
+    public function changeIngameState(stateName) {
+        engine.updateComplete.addOnce(function() {
+            ingameEsm.changeState(stateName);
+        });
+    }
+
+    public function changeUiState(stateName) {
+        uiEngine.updateComplete.addOnce(function() {
+            uiEsm.changeState(stateName);
+            var pageName = statePageMap.get(stateName);
+
+            if(pageName != null) {
+                pages.showPage(pageName);
+            }
+        });
+    }
+
+    public function delay(func, time_s:Float) {
+        haxe.Timer.delay(func, Std.int(time_s * 1000 / timeFactor));
+    }
+}
